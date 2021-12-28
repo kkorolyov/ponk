77aee8d69676d41d10bfa8feb18c76843dfd79da
kkorolyov <kirillk753@gmail.com>
2021-12-28 15:46:50 -0800

    Change publish flow again

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 	id("org.javamodularity.moduleplugin") version "1.+"
 	id("org.beryx.jlink") version "2.+"
 	id("org.ajoberstar.reckon") version "0.+"
-	id("com.github.breadmoirai.github-release") version "2.+"
 }
 
 tasks.wrapper {
@@ -76,28 +75,13 @@ jlink {
 	forceMerge("kotlin", "slf4j", "log4j", "jackson")
 
 	jpackage {
-		icon = "pancake.ico"
+		appVersion = (findProperty("jpackage.version") ?: version).toString()
+
+		icon = if (Os.isFamily(Os.FAMILY_WINDOWS)) "pancake.ico" else "pancake.png"
 
 		val options = mutableListOf("--license-file", "LICENSE")
 		if (Os.isFamily(Os.FAMILY_WINDOWS)) options += "--win-dir-chooser"
 
 		installerOptions = options.toList()
 	}
-}
-
-githubRelease {
-	token(System.getenv("GITHUB_TOKEN"))
-	owner("kkorolyov")
-
-	tagName(version.toString())
-
-	body(changelog())
-
-	releaseAssets(
-		fileTree("${buildDir}/jpackage").include("*.*"),
-		fileTree("${buildDir}/libs").include("*.jar")
-	)
-}
-tasks.githubRelease {
-	dependsOn(tasks.jpackage)
 }
